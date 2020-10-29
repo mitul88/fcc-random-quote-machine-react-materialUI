a4e3653ceebd4ce76075a24f9831eaf460c29997
shahed <shahed.rahman88@gmail.com>
2020-10-29 21:40:18 +0600

    modify react app to align with current project requirement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { random } from 'lodash';
+import 'typeface-roboto';
+import QuoteMachine from './components/QuoteMachine'
+import Grid from '@material-ui/core/Grid'
+import {withStyles} from '@material-ui/core/styles'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems : 'center'
+
+  }
 }
 
-export default App;
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex : null
+    }
+    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+    this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
+  }
+
+  componentDidMount(){
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes: quotes }, this.assignNewQuoteIndex ));
+  }
+
+  get selectedQuote(){
+    if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)){
+      return undefined;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  selectQuoteIndex(){
+    if(!this.state.quotes.length){
+      return undefined;
+    }
+    return random(0, this.state.quotes.length - 1);
+  }
+
+  assignNewQuoteIndex() {
+    this.setState({selectedQuoteIndex : this.selectQuoteIndex() })
+  }
+
+  render() {
+    return (
+    <Grid className={this.props.classes.container} id = 'quote-box' justify='center' container>
+      <Grid xs={11} lg={8} item>
+       {
+         this.selectedQuote ? <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+         : null
+        }
+      </Grid>
+    </Grid>
+  );
+}
+}
+
+export default withStyles(styles)(App);
---
+++ src/components/QuoteMachine.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Card from '@material-ui/core/Card'
+import Button from '@material-ui/core/Button'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faTwitter} from '@fortawesome/free-brands-svg-icons'
+
+
+const QuoteMachine = (props) => {
+    return (
+        <Card>
+            <CardContent>     
+                 <Typography id="text">
+                            {props.selectedQuote.quote} -<span id='author'>{props.selectedQuote.author}</span> 
+                 </Typography> 
+            </CardContent>    
+            <CardActions>
+                <Button id="new-quote" size='small' onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+                <IconButton
+                    id="tweet-quote"
+                    target="_blank"
+                    href={encodeURI(`https://twitter.com/intent/tweet?text=${props.selectedQuote.quote}+  -+${props.selectedQuote.author}&hashtags=neverGiveUp`)}
+                >
+                    <FontAwesomeIcon icon={faTwitter} size='md'></FontAwesomeIcon>
+                </IconButton>
+            </CardActions>                       
+        </Card>
+    )
+}
+
+export default QuoteMachine; 
